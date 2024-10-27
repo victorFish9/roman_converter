787163ef56cfd92305b3f5aef686fdff54f3bcc4
Victor <aue228lublupacanov@gmail.com>
2024-10-27 14:58:02 +0100

    ci/cd created

--- roman_converter/urls.py
+++ roman_converter/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('converter/', include('converter.urls'))
+    path('converter/', include('converter.urls')),
 ]
